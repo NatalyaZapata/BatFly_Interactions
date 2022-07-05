b53a5aff9481a2c855dee12ecafc707508ac4862
NatalyaZapata <zapatamesan@gmail.com>
2022-07-04 23:18:04 -0300

    Update figure 5

--- Figure 5.R
+++ Figure 5.R
@@ -1,6 +1,6 @@
 #Figure 5. Richness of bat flies (Nycteribiidae and Streblidae) per bat family
 #Import data sets
-
+data1<-read.csv("BatFly_Species.csv", sep=",")
 data2<-read.csv("BatFly_Bat_Pop.csv", sep=",")
 data3<-read.csv("BatFly_Fly_Pop.csv", sep=",")
 
@@ -10,12 +10,14 @@ flyfam<-unique(cbind(data3$FlyFamily, data3$CurrentFlySpecies))
 which(table(flyfam[,2])>1)
 
 family<-NULL
-for (i in 1:length(data1$CurrentBatSpecies)){#assing families to bat species in data2
+for (i in 1:length(data1$CurrentBatSpecies)){#passing families to bat species in data2 
   
   family[[i]]<-fam[which(fam[,2]==data1$CurrentBatSpecies[i]),1]
 }
 
 family<-unlist(family)
+length(family)
+
 
 flyfamily<-NULL
 for (i in 1:length(data1$CurrentFlySpecies)){#assing families to fly species in data3
@@ -27,10 +29,15 @@ flyfamily<-unlist(flyfamily)
 
 length(flyfamily)
 
+
 plotdata<-unique(cbind(data1$CurrentFlySpecies,family,flyfamily))
 
 plotdata<-as.matrix(table(plotdata[,2],plotdata[,3]))#the number of times a family name is repeated indicates parasite richness
+
 plotdata<-plotdata[order(rowSums(plotdata)),]
+
+plotdata<-cbind(Nycteribiidae=(plotdata[,1]+plotdata[,2]), 
+                Streblidae= plotdata[,3]) #Provisionalmente
 
 #Plot
 
@@ -38,8 +45,13 @@ png("Figure_5.png", res = 300,
     width = 2100, height = 2000, unit = "px")
 par(las=1, mar=c(4, 8, 1, 2))
 
-barplot(t(plotdata), horiz=T, xlim=c(0,200), xlab="Parasite richness", col=c("#E5EFC1","#E5EFC1", "#39AEA9"))
+bar<-barplot(t(plotdata/sum(plotdata)), horiz=T, xlim=c(0,0.5), xlab="Relative parasite richness", col=c("#E5EFC1", "#39AEA9"))
 
-legend(x=120, y=15, legend=colnames(plotdata), pch=19, pt.cex=1.5,col=c("#ffffff","#E5EFC1", "#39AEA9"),bty = "n")
+legend(x=0.25, y=15, legend=colnames(plotdata), pch=19, pt.cex=1.5,
+       col=c("#E5EFC1", "#39AEA9"),bty = "n", x.intersp=0.1, y.intersp=0.5)
+
+text(y=bar, x=colSums(t(plotdata/sum(plotdata)))+0.02  , (plotdata[,1]+plotdata[,2]),cex=0.9)
+
+
 
 dev.off()
