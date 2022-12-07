91ae562ac25f0c86dfb80de59412e421862fc38b
NatalyaZapata <107432532+NatalyaZapata@users.noreply.github.com>
2022-12-07 15:25:07 -0300

    update

--- code/Figure 2.R
+++ code/Figure 2.R
@@ -54,7 +54,7 @@ for (i in 1:length(seq(1904,2022, by=1))){
   dy[i]<-sum(seq(1904,2022, by=1)[i]==years$Var1)
 }
 
-miss.year<-cbind(seq(1904,2022, by=1), dy)[which(cbind(seq(1904,2022, by=10), dy)[,2]==0),1]
+miss.year<-cbind(seq(1904,2022, by=1), dy)[which(cbind(seq(1904,2022, by=1), dy)[,2]==0),1]
 miss.year<-data.frame(Var1=sapply(miss.year, as.factor), Freq=rep(0, length(miss.year)))
 all.years<-rbind(years, miss.year)
 
@@ -65,18 +65,20 @@ class(yeardata)
 str(yeardata)
 head(yeardata)
 tail(yeardata)
-
-
+nrow(yeardata)
+View(yeardata)
 ######################### 2. PLOTTING ######################################
 
 
 png("figures/Figure_2.png", res = 300,
     width = 3000, height = 2000, unit = "px")
 
-par(las=2, mar=c(5, 5, 4, 2))
+par(las=1, mar=c(5, 5, 4, 2))
 barplot(yeardata$Freq, names.arg=yeardata$Var1, ylim=c(0,16),
-        xlab="Year of publication", ylab="Number of studies",
-        xaxp=c(1904, 2022, 59))
+        xlab="Year of publication", ylab="Number of studies")
+        #xaxp=c(1904, 2022,59), xaxt='n')
+
 
 dev.off()
+
 
